31606d034036a05320a86de05b02a1817cd368f6
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-04-02 22:55:31 +0200

    Add files via upload

--- greenhouse.py
+++ greenhouse.py
@@ -3,6 +3,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# Plot style.
+plt.style.use(['ggplot', 'dark_background'])
 # Import standard libraries.
 import os
 import datetime
@@ -37,17 +39,35 @@ class green_house():
         self.current_temperature = 0
         # Is the sun out?
         self.is_sunny = True
+        # Temperature due to sun.
+        self.sun_temperature = 0
         # Vent temperature effect.
         self.vent_temperature_effect = 0
         # Heating effect.
         self.heating_temperature = 0
+        # Fogging temperature effect.
+        self.fogging_temperature_effect = 0
+        # Base NN value.
+        self.nn_val = 1
+        # Base NN val for vents.
+        self.vent_nn_val = 1
+        # Base NN val for heating.
+        self.heating_nn_val = 1
+        # Base NN val for screen,
+        self.screen_nn_val = 1
+        # Resource usage.
+        self.resources_used = 0
+        self.resources_used_per_harvest = []
+        # Hours for harvest to occur.
+        self.hours_for_harvest = 0
+        self.harvest_times = []
         # gdd requirement for cucumbers
-        if crop == 'cucumber':
+        if self.crop == 'cucumber':
             self.base_temp = 15.5
-            self.max_temp = 32
-            self.gdd_req = 482
-            
-
+            self.max_temp = 28
+            self.too_hot = 36
+            # gdds turned into hours required. (Growing degree hours).
+            self.gdd_req = 11568
         
     def set_up_data(self, dataset='weather_data.csv'):
         '''
@@ -66,13 +86,15 @@ class green_house():
         # Change temperates from 0.1C to standard units
         df['   TX'] = df['   TX'] / 10
         df['   TN'] = df['   TN'] / 10
+
+        # Change time format.
         df = df.set_index('YYYYMMDD')
         # Plot temperature vs date.
 #        plt.plot(df.YYYYMMDD, df['   TX'])
         
         # Create time dataframe for greenhouse temperature.
         greenhouse_df = pd.DataFrame({'datetimes': pd.date_range('2010-3-01T00:00:00.000Z',
-                                                                 '2017-3-01T00:00:00.000Z',
+                                                                 '2016-3-01T00:00:00.000Z',
                                                                  freq='H'),
                                                                  'temperature': np.nan})
         greenhouse_df_max_temp = pd.DataFrame({'datetimes': pd.date_range('2010-3-01T15:00:00.000Z',
@@ -115,8 +137,18 @@ class green_house():
         greenhouse_df = greenhouse_df.interpolate(method='linear')
         # Compute average temperature over last 5 hours.
         greenhouse_df['5_hour_temp_avg'] = pd.rolling_mean(greenhouse_df['temperature'], window=5, min_periods=1)
-
-        
+        # Add growing degree days column.
+        greenhouse_df['growing_degree_days'] = 0
+        # Add is sunny column. Only currently says 12 hours a day is sunny/non sunny.
+        greenhouse_df['is_sunny'] = True
+        for i, indx in enumerate(greenhouse_df.index):
+            indx = pd.Timestamp(indx)
+            hour = int(indx.hour)
+            if hour > 6 and hour <= 18:
+                greenhouse_df['is_sunny'][i] = True
+            else:
+                greenhouse_df['is_sunny'][i] = False
+                
         # Plot new temp values with time
         plt.plot(greenhouse_df.index, greenhouse_df.temperature)
         plt.show()
@@ -133,28 +165,6 @@ class green_house():
         self.greenhouse_df = greenhouse_df.dropna()
         
         return df, greenhouse_df
-    
-    def growing_degree_days(self, daily_max_temp=20):
-        if self.crop == 'cucumber':
-            '''
-            Formula and temperatures obtained from: 
-            http://horttech.ashspublications.org/content/6/1/27.short
-            
-            gdd requirement for cucumbers from:
-            http://nwhortsoc.com/wp-content/uploads/2016/01/Andrews-Croptime-1.pdf,
-            adjusted to 900 for current formula with max temperature
-            '''           
-            
-            if self.daily_max_temp >= self.max_temp:
-                self.gdd += (self.max_temp - (self.daily_max_temp - self.max_temp)) - self.base_temp
-                
-            elif self.daily_max_temp <= self.base_temp:
-                self.gdd += 0
-                
-            else:
-                self.gdd += self.daily_max_temp - self.base_temp
-        
-        return self.gdd
         
     def water_storage_usage(self, daily_rainfall=10, tank_size=100, irrigation=True, irrigation_rate=2):
         '''
@@ -181,15 +191,6 @@ class green_house():
             self.water_supply -= self.irrigation_rate
     
         return self.water_supply
-        
-    def greenhouse_temperature(self):
-        '''
-        Calculates the temperature in a greenhouse by hour based on daily temperature
-        values.
-        '''
-        # Find a greenhouse temperature function.
-        self.greenhouse_temp = 0
-        return self.greenhouse_temp
     
     def soil_water_content(self):
         '''
@@ -204,7 +205,6 @@ class green_house():
         '''
         Controls opening and closing of vents, turning on of irrigation etc.
         '''
-        self.nn_val = 0
         self.vent_threshold = 0.5
         self.heating_threshold = 0.5
         self.screen_threshold = 0.5
@@ -213,17 +213,17 @@ class green_house():
         self.carbon_dioxide_threshold = 0.5
         self.irrigation_threshold = 0.5
 
-        if self.nn_val >= self.vent_threshold:            
+        if self.vent_nn_val >= self.vent_threshold:            
             self.vents = True
         else:
             self.vents = False
         
-        if self.nn_val >= self.heating_threshold:
+        if self.heating_nn_val >= self.heating_threshold:
             self.heating = True
         else:
             self.heating = False
             
-        if self.nn_val >= self.screen_threshold:
+        if self.screen_nn_val >= self.screen_threshold:
             self.screens = True
         else:
             self.screens = False
@@ -253,17 +253,16 @@ class green_house():
         Effect of vents being opened or closed on the temperature within the
         greenhouse.
         '''
+        # Reset vent change
+        self.vent_temperature_effect = 0.0
+        
         if self.vents:
             if self.current_temperature >= self.outside_temp and self.is_sunny:
-                self.vent_temperature_effect = self.vent_temperature_effect
+                self.vent_temperature_effect -= 1.0
             elif self.current_temperature >= self.outside_temp and not self.is_sunny:
-                self.vent_temperature_effect -= 3
+                self.vent_temperature_effect -= 3.0
         
-        else:
-            if self.is_sunny:
-                self.vent_temperature_effect += 3
-            else:
-                self.vent_temperature_effect += 1
+
         return self.vent_temperature_effect
                 
     def heating_effect(self):
@@ -272,10 +271,11 @@ class green_house():
         greenhouse.
         '''
         if self.heating:
-            self.heating_temperature += 1
+            self.heating_temperature += 2.0
+            self.resources_used += 1
         else:
-            self.heating_temperature = 0
-
+            self.heating_temperature = 0.0
+        
         return self.heating_temperature
                 
     def screen_effect(self):
@@ -284,12 +284,28 @@ class green_house():
         '''
         if self.screens:
             self.is_photosynthesizing = False
-            self.insulation = 0.5 # 0.5 * heat loss from inside / heat gain from outside.
+            self.insulation = 0.2 # 0.2 * heat loss from inside / heat gain from outside.
         else:
             self.insulation = 1.0
 
         return self.insulation
-
+        
+    def sun_effect(self):
+        '''
+        The effect of sunlight upon greenhouse temperature.
+        '''
+        if self.is_sunny:
+            self.sun_temperature += 1
+#        elif self.is_sunny == False and self.current_temperature <= (self.outside_temp + 4):
+#            self.sun_temperature -= 4
+        elif self.current_temperature > self.outside_temp + 4 and not self.is_sunny:
+            self.sun_temperature -= 4
+            # Sun temperature can't go below 0.
+            if self.sun_temperature < 0:
+                self.sun_temperature = 0
+        
+        return self.sun_temperature
+        
     def lighting_effect(self):
         '''
         The effect that lighting has on plant growth in the greenhouse.
@@ -301,8 +317,14 @@ class green_house():
         '''
         The effect fogging has on plant growth.
         '''
-        pass
-
+        if self.fogging:
+            self.resources_used += 1
+            self.fogging_temperature_effect = -6
+        else:
+            self.fogging_temperature_effect = 0
+            
+        return self.fogging_temperature_effect
+        
     def irrigation_effect(self):
         '''
 
@@ -316,31 +338,137 @@ class green_house():
         if self.carbon_dioxide_input:
             self.photosynthesis_rate *= 1.1
 
-    def calculate_greenhouse_temperature(self):
+    def calculate_greenhouse_temperature(self, policy='basic'):
         '''
         Calculates the temperature in the greenhouse based on outside and controlled factors.
+        
+        Policies include: random, qlearning, markov and basic.
         '''
         self.greenhouse_df['greenhouse_temperature'] = 0
         
         for i, value in enumerate(self.greenhouse_df.index):
-            # Set the threshold value.
-            self.nn_val = np.random.uniform(0, 1)
+            # Completely random policy
+            if policy == 'random':
+                # Set the threshold value, this is with random control.
+                self.nn_val = np.random.uniform(0, 1)
+                self.screen_nn_val = np.random.uniform(0, 1)
+                self.heating_nn_val = np.random.uniform(0, 1)
+                self.vent_nn_val = np.random.uniform(0, 1)
+            # Naive policy.
+            elif policy == 'basic':
+                if self.current_temperature > self.max_temp:
+                    self.nn_val = 1
+                    self.screen_nn_val = 1
+                    self.vent_nn_val = 1
+                    self.heating_nn_val = 0
+                elif self.current_temperature <= self.base_temp:
+                    self.nn_val = 0
+                    self.screen_nn_val = 1
+                    self.vent_nn_val = 0
+                    self.heating_nn_val = 1
+            
+            self.greenhouse_control_evaluation()
+            # Is it sunny this hour?
+            self.is_sunny = self.greenhouse_df['is_sunny'][i]
+    
             if i % 10000 == 0:
-                print(self.heating_effect(), self.screen_effect(), self.vent_effect())
-
+                print(self.vents, self.heating, self.screens, self.carbon_dioxide_input)
+                print(self.heating_effect(), self.screen_effect(), self.vent_effect(), self.sun_effect())
+                print(self.current_temperature, self.outside_temp)
             if i == 0:
-                self.greenhouse_df['greenhouse_temperature'][value] = self.greenhouse_df['5_hour_temp_avg'][value] + ((self.vent_effect() + self.heating_effect()) * self.screen_effect())
+                # Initial value for greenhouse temperature.
+                self.greenhouse_df['greenhouse_temperature'][i] = self.greenhouse_df['5_hour_temp_avg'][i] + \
+                self.fogging_effect() + self.vent_effect() + ((self.heating_effect() + self.sun_effect()) * self.screen_effect())
+                
             else:
-                self.greenhouse_df['greenhouse_temperature'][i] =((self.greenhouse_df['greenhouse_temperature'][i - 1] + self.greenhouse_df['5_hour_temp_avg'][i]) / 2) + (self.vent_effect() + \
-                self.heating_effect()) * self.screen_effect()
+                # Any value for greenhouse temperature past the first row.
+                self.greenhouse_df['greenhouse_temperature'][i] =((self.greenhouse_df['greenhouse_temperature'][i - 1] + \
+                self.greenhouse_df['5_hour_temp_avg'][i]) / 2) + self.vent_effect() + \
+                self.fogging_effect() + (self.heating_effect() + self.sun_effect()) * self.screen_effect()
+            # Pass back the temperature to the functions.
+            self.current_temperature = self.greenhouse_df['greenhouse_temperature'][i]
+            # Pass back outside temperature.
+            self.outside_temp = self.greenhouse_df['5_hour_temp_avg'][i]
+        # Plot the greenhouse temperature.
+        plt.plot(self.greenhouse_df['greenhouse_temperature'], alpha=0.2)
+        plt.plot(self.greenhouse_df['5_hour_temp_avg'], alpha=1, color='red')
+        plt.grid('off')
+        plt.xlabel('Date')
+        plt.ylabel('Temperature')
+        plt.legend()
+        plt.show()
+        # Plot the same over a shorter range to see daily variation.
+        plt.plot(self.greenhouse_df['greenhouse_temperature'][:200], alpha=0.2)
+        plt.plot(self.greenhouse_df['5_hour_temp_avg'][:200], alpha=1, color='red')
+        plt.grid('off')
+        plt.xlabel('Date')
+        plt.ylabel('Temperature')
+        plt.legend()
+        plt.show()
 
         return self.greenhouse_df
+        
+    def calculate_gdd(self):
+        '''
+        Formula and temperatures obtained from: 
+        http://horttech.ashspublications.org/content/6/1/27.short
+            
+        gdd requirement for cucumbers from:
+        http://nwhortsoc.com/wp-content/uploads/2016/01/Andrews-Croptime-1.pdf,
+        adjusted to 900 for current formula with max temperature
+        '''   
+        # Calculate hourly gdd counts.
+        for i, temp in enumerate(self.greenhouse_df['greenhouse_temperature']):
+            # For temperatures over the comfortable growing temperatures.
+            if temp >= self.max_temp and temp < self.too_hot:
+                # Still can grow because it isn't too hot.
+                self.gdd += (self.max_temp - (temp - self.max_temp)) - self.base_temp
+            # Can't grow as it is too hot.
+            elif temp >= self.too_hot:
+                self.gdd += 0
+            
+            # For temperatures less than comfortable growing conditions. 
+            elif temp <= self.base_temp:
+                self.gdd += 0
+            
+            # For comfortable growing temperatures
+            else:
+                self.gdd += temp - self.base_temp
+            
+            self.greenhouse_df['growing_degree_days'][i] = self.gdd
 
+            if self.gdd >= self.gdd_req:
+                self.gdd = 0
+                self.harvest_times.append(self.hours_for_harvest)
+                self.hours_for_harvest = 0
+            else:
+                self.hours_for_harvest += 1
+                
+        # Visualise the data.
+        plt.plot(self.greenhouse_df['growing_degree_days'])
+        plt.xlabel('Date')
+        plt.ylabel('Growing degree hours')
+        plt.title('Growing degree hours over time in greenhouse')
+        plt.grid('off')
+        axes = plt.gca()
+        axes.set_ylim([0, 12000])
+        plt.show()
+        
+        # How many resources were used per harvest
+        print(self.resources_used_per_harvest, self.harvest_times)
+        
+        return self.greenhouse_df
+        
     def calculate_greenhouse_production(self):
         pass
 
     def calculate_reward(self):
-        pass
+        '''
+        Reward function is based on both resource use and time to harvest. Should
+        probably be some points for a more stable temperature too. 0 is the perfect
+        score albiet impossible, the closer to 0 the better.
+        '''
+        return -np.mean(self.harvest_times) - self.resources_used/len(self.harvest_times)
 
     def DeepQNetwork(self):
         pass
@@ -350,5 +478,5 @@ df, greenhouse  = x.set_up_data()
 print(greenhouse['5_hour_temp_avg'])
 x.greenhouse_control_evaluation()
 temps = x.calculate_greenhouse_temperature()
-print(temps['greenhouse_temperature'])
-plt.plot(temps['greenhouse_temperature'])
+gdds = x.calculate_gdd()
+reward = x.calculate_reward()
